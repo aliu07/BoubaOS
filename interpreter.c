e7cc40f5a44be14f78a557e8961a556f610883a7
Alexander Liu <alexander_l_22@hotmail.com>
2025-01-26 15:46:02 -0500

    Update help command

--- interpreter.c
+++ interpreter.c
@@ -195,13 +195,20 @@ int interpreter(char* command_args[], int args_size) {
 
 int help() {
 
-    // note the literal tab characters here for alignment
-    char help_string[] = "COMMAND			DESCRIPTION\n \
-help			Displays all the commands\n \
-quit			Exits / terminates the shell with “Bye!”\n \
-set VAR STRING		Assigns a value to shell memory\n \
-print VAR		Displays the STRING assigned to VAR\n \
-run SCRIPT.TXT		Executes the file SCRIPT.TXT\n ";
+    char help_string[] =
+       "COMMAND          DESCRIPTION\n"
+       "help             Displays all the commands\n"
+       "quit             Exits / terminates the shell with \"Bye!\"\n"
+       "set VAR STRING   Assigns a value to shell memory\n"
+       "print VAR        Displays the STRING assigned to VAR\n"
+       "run SCRIPT.TXT   Executes the file SCRIPT.TXT\n"
+       "echo STRING      Displays STRING. If STRING starts with $, prints the value of the variable\n"
+       "my_ls            Lists the contents of the current directory\n"
+       "my_mkdir DIR     Creates a new directory with name DIR\n"
+       "my_touch FILE    Creates a new empty file with name FILE\n"
+       "my_cd DIR        Changes current directory to DIR\n"
+       "my_fork CMD      Executes CMD with ARGS using fork-exec pattern\n";
+
     printf("%s\n", help_string);
     return 0;
 }
